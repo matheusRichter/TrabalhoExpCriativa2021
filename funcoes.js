3d28e448c9138d040655bb7da43c9f165d45ae22
Matheus Richter <matheusrichter007@gmail.com>
2021-05-17 22:03:20 -0300

    exibicao de produtos a repor - bug de duplicidade

--- funcoes.js
+++ funcoes.js
@@ -1,19 +1,29 @@
 /*Variáveis e constantes*/
 let produtos = [];
+let repor = [];
 let contador = 1;
 const form = document.getElementById('form-cadastro');
 const table = document.getElementById('content-table');
+const gastos = document.getElementById('gastos');
+const reposicao = document.getElementById('reposicao');
+const tableR = document.getElementById('reposition-table');
+const modalTitle = document.getElementById('modal-title');
 
 /*Funções de cadastro e exibição de produtos na tabela*/
+function mostrarFormCadastro() {
+    modalTitle.innerText = 'Cadastro de Produto';
+    form.style.display = 'block';
+    gastos.style.display = 'none';
+    reposicao.style.display = 'none';
+}
+
 function cadastrar(form){
     let dataVal = form.validade.value;
     dataVal = dataVal.split('-');
     dataVal = dataVal[2] + '/' + dataVal[1] + '/' + dataVal[0];
-    produtos.push({"Nome":form.nome.value, "Categoria":form.categoria.value, "Marca":form.marca.value, "Preco":form.preco.value, "validade":dataVal, "Quantidade":form.quantidade.value});
+    produtos.push({"Nome":form.nome.value, "Categoria":form.categoria.value, "Marca":form.marca.value, "Preco":form.preco.value, "validadeM":dataVal, validade:form.validade.value ,"Quantidade":form.quantidade.value});
 
     addToTable();
-
-    //alert(produtos.length);
 }
 
 function addToTable(){
@@ -26,7 +36,7 @@ function addToTable(){
     let c5 = linha.insertCell(4);
 
     c1.innerHTML = produtos[produtos.length-1].Nome;
-    c2.innerHTML = produtos[produtos.length-1].validade;
+    c2.innerHTML = produtos[produtos.length-1].validadeM;
     c3.innerHTML = produtos[produtos.length-1].Quantidade;
     c4.innerHTML = produtos[produtos.length-1].Preco;
     c5.innerHTML = produtos[produtos.length-1].Marca;
@@ -40,3 +50,33 @@ form.addEventListener(
         event.preventDefault();
     }
 );
+
+/*Funções de exibição de produtos que nesecitam de reposição*/
+function listarReposicoes() {
+    produtos.forEach(element => {
+        if ((Date.parse(element.validade) < Date.now() || element.Quantidade <= 1) && !repor.includes(element)) {
+            repor.push(element);
+        }
+    });
+}
+
+function mostrarReposicoes() {
+    listarReposicoes();
+    for (let i = 1; i < tableR.rows.length; i++) {
+        tableR.deleteRow(i);
+    }
+    for (let i = 0; i < repor.length; i++) {
+        const linha = tableR.insertRow(i+1);
+        let c1 = linha.insertCell(0);
+        let c2 = linha.insertCell(1);
+        let c3 = linha.insertCell(2);
+
+        c1.innerHTML = repor[i].Nome;
+        c2.innerHTML = repor[i].validadeM;
+        c3.innerHTML = repor[i].Quantidade;
+    }
+    modalTitle.innerText = 'Produtos a Repor';
+    form.style.display = "none";
+    gastos.style.display = "none";
+    reposicao.style.display = "block";
+}
